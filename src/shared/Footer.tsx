9328c7bc77f88e4e19ae4212c8d097fca881225b
Muhammad-Hamim <hamim.ml01@gmail.com>
2024-08-29 16:27:21 +0800

    change the footer text

--- src/shared/Footer.tsx
+++ src/shared/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-[#1F3D7A] text-white py-6 text-center">
       <div className="container mx-auto px-4">
-        <p>&copy; 2023 Online Voting App</p>
+        <p>&copy; 2024 Online Voting App</p>
       </div>
     </footer>
   );
